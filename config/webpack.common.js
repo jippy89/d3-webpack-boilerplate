89527e58ea253dba992a3124ddd203a86a95867d
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-14 16:25:16 +0700

    feat: initiate d3 webpack boilerplate

---
+++ config/webpack.common.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/'
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: '/index.html'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { select } from 'd3'
+
+const svg = select('body')
+
+console.log(svg)
