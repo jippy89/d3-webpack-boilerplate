22d45afcbe3c053bb0b096bda82e9f36cf9a01bd
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-15 17:00:21 +0700

    feat: add ability to add polar chart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
-import { select } from 'd3'
+import * as d3 from 'd3'
+import './index.css'
 
-const svg = select('body')
+// Organize data
+const reMap = function(oldValue) {
+  var oldMin = 0,
+    oldMax = -359,
+    newMin = 0,
+    newMax = (Math.PI * 2),
+    newValue = (((oldValue - 90 - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+  return newValue;
+}
 
-console.log(svg)
+const data = [
+  [reMap(25), 1, 20, 'label 1'],
+  [reMap(105), 0.8, 10, 'label 2'],
+  [reMap(266), 1, 8, 'label 3'],
+  [reMap(8), 0.2, 22, 'label 4'],
+  [reMap(189), 1, 28, 'label 5'],
+  [reMap(350), 0.6, 15, 'label 6'],
+  [reMap(119), 0.4, 24, 'label 7'],
+  [reMap(305), 0.8, 31, 'label 8']
+];
+console.log('data', data);
+
+// Draw the polar chart
+const width = 600
+const height = 600
+const radius = Math.min(width, height) / 2 - 30
+
+const r = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, radius])
+
+const svg = d3.select('body')
+  .append('svg')
+  // .call(zoom)
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+const gr = svg
+  .append('g')
+  .attr('class', 'r axis')
+  .selectAll('g')
+  .data(r.ticks(5).slice(1))
+  .enter()
+  .append('g')
+
+gr.append('circle')
+  .attr('r', r);
+
+const ga = svg.append('g')
+  .attr('class', 'a axis')
+  .selectAll('g')
+  .data(d3.range(0, 360, 30)) // line density
+  .enter().append('g')
+  .attr('transform', function(d) {
+    return 'rotate(' + -d + ')';
+  });
+
+ga.append('line')
+  .attr('x2', radius);
+// End of adding polar chart
