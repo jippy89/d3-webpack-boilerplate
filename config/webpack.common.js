f04f00eec0862a34a13f1ab2a9aea61fe7d22eaa
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-15 16:56:29 +0700

    feat: add ability to load css

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,6 +23,11 @@ module.exports = {
             plugins: ['@babel/plugin-transform-runtime']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
